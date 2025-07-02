e63e195ccc1d8d9bf7f8a5df58f7ca0f4b99b83f
Orange <orange-cpp@yandex.ru>
2025-07-02 09:23:47 +0300

    Removes unused function
    Removes the `add_right_label` function from the entity overlay render class, as it is no longer being used.

--- include/ohud/entity_overlay_render.hpp
+++ include/ohud/entity_overlay_render.hpp
@@ -17,8 +17,6 @@ namespace ohud
         void add_2d_box(const ImColor& box_color, const ImColor& fill_color = ImColor{0.f, 0.f, 0.f, 0.f},
                         float thickness = 1.f) const;
 
-        void add_right_label(const ImColor& color, const std::string_view& text) const;
-
         void add_right_bar(const ImColor& color, const ImColor& outline_color, const ImColor& bg_color, float width,
                            float ratio, float offset = 5.f);
         void add_left_bar(const ImColor& color, const ImColor& outline_color, const ImColor& bg_color, float width,
